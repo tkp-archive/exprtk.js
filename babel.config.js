492bfad8743fd74450591f1a559f24b51921f2f1
Tim Paine <t.paine154@gmail.com>
2021-11-26 23:08:25 -0500

    fix copyright notice on 2 files

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 /* ***************************************************************************
  *
- * Copyright (c) 2021, the perspective-studio authors.
+ * Copyright (c) 2021, the exprtk.js authors.
  *
- * This file is part of the perspective-studio library, distributed under the terms of
+ * This file is part of the exprtk.js library, distributed under the terms of
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
